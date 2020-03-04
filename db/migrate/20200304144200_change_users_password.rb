1e8cf5919c0bcbe447c97b52593c2d3aaa11275c
Babyyodabc <rjwahba@gmail.com>
2020-03-04 12:07:48 -0500

    DB changes, routes, application layout, sessions and application controller

--- db/migrate/20200304144200_change_users_password.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUsersPassword < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :users, :password, :password_digest 
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_03_04_144503) do
-=======
-ActiveRecord::Schema.define(version: 2020_03_04_144200) do
->>>>>>> master
 
   create_table "follow_goals", force: :cascade do |t|
     t.integer "follower_id"
